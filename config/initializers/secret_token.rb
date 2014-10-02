fff3ff04372845c1f51e9848044ec07eb9f91de0
bryron <bryron93@live.com>
2014-10-02 10:10:50 -0300

    start of new ch4 walk-through

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Derp::Application.config.secret_token = 'e3ce99e97a35659fefaaf02dbc041022a50f6487c1be7dade5af9e95fc1132259603722d9ddd813a93feb84443067e9e7c69aed4b650e224b311900c9de7d0cb'
